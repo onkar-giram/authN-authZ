f68686b8e79be71a18bd357926fa292df0ba0a93
onkar-giram <meonkargiram@gmail.com>
2023-11-07 10:55:13 +0530

    login & signup funtions ready

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   try {
@@ -45,4 +46,65 @@ exports.signup = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the details carefully",
+      });
+    }
+
+    //check for user
+    let user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User Not Registered",
+      });
+    }
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+
+    //verify user
+    if (await bcrypt.compare(password, user.password)) {
+      //password match
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+      // user = user.toObject();
+      user.token = token;
+      user.password = undefined;
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.cookie("token", token, options).status(200).json({
+        status: true,
+        token,
+        user,
+        message: "User logged in successfully",
+      });
+    } else {
+      //password not matched
+      return res.status(403).json({
+        success: false,
+        message: "Password incorrect",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `Error while loggin up: ${err}`,
+    });
+  }
+};
